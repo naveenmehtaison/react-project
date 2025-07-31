24cce876b9a5a1c153c8e78be9cc13ba427bdfef
naveenmehtaison <naveenmehta2024@outlook.com>
2025-07-31 13:00:04 +0530

    added cursor effect

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
-
+import AnimatedCursor from "react-animated-cursor";
 export default function App() {
   return (
     <main className="text-gray-400 bg-black body-font">
@@ -15,6 +15,15 @@ export default function App() {
       <Skills />
       <Testimonials />
       <Contact />
+      <AnimatedCursor
+        innerSize={16}
+        outerSize={30}
+        color="59, 130, 246"
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={5}
+        trailingSpeed={20}
+      />
     </main>
   );
 }
